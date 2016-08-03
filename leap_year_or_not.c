0cac11e79a9557bc496720cacf38be6718fa6946
sowmiyayoganathan <sowmiyayoganathan@gmail.com>
2016-08-03 20:34:58 +0530

    Update leap_year_or_not.c

--- leap_year_or_not.c
+++ leap_year_or_not.c
@@ -3,9 +3,14 @@
 int main(void) {
 	int year;
 	scanf("%d",&year);
-	if(((year%4==0)&&(year%100!=0)) || (year%400==0) || (year==1752))
-		printf("%d is a leap year",year);
-	else 
-		printf("%d is not a leap year",year);
+	if(year>0)
+	{
+		if(((year%4==0)&&(year%100!=0)) || (year%400==0) || (year==1752))
+			printf("%d is a leap year",year);
+		else 
+			printf("%d is not a leap year",year);
+	}
+	else
+		printf("Invalid year");
 	return 0;
 }
